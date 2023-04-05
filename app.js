2cb112cda3ebfe4796af7494909e8179e9eda7e2
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-04-05 23:33:45 +0600

    basis scema

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { Schema } = require("mongoose");
 
-
+// middleware
 app.use(express.json());
 app.use(cors());
 
+// schema design
 
+const productsSchema = Schema({
+  name:{
+    type: String,
+    required: [true,"please select a product name"],
+    trim:true, //remove specing
+    unique: true,
+    minLength:[4, "name must be minimum 4 characters"],
+    maxLength:[30, "name must be maximum 30 characters"],
+  },
+  description:{
+    type:String,
+    required:true,
+  },
+  price:{
+    type:Number,
+    required:true,
+    min:[0 , "price must be at least 0"],
+  },
+  unit:{
+    type:String,
+    required:true,
+    enum:["KG", "Ltr", "Pcs"]
+
+  },
+})
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
