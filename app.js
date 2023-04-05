4279b32e73fd53d38a2f2c9f835e25e618cc5e5d
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2023-04-06 00:37:39 +0600

    done scheme

--- app.js
+++ app.js
@@ -10,29 +10,76 @@ app.use(cors());
 // schema design
 
 const productsSchema = Schema({
-  name:{
+  name: {
     type: String,
-    required: [true,"please select a product name"],
-    trim:true, //remove specing
-    unique: true,
-    minLength:[4, "name must be minimum 4 characters"],
-    maxLength:[30, "name must be maximum 30 characters"],
+    required: [true, "Please provide a name for this product."],
+    trim: true,
+    unique: [true, "Name must be unique"],
+    minLength: [3, "Name must be at least 3 characters."],
+    maxLenght: [100, "Name is too large"],
   },
-  description:{
-    type:String,
-    required:true,
+  description: {
+    type: String,
+    required: true
   },
-  price:{
-    type:Number,
-    required:true,
-    min:[0 , "price must be at least 0"],
+  price: {
+    type: Number,
+    rquired: true,
+    min: [0, "Price can't be negative"],
   },
-  unit:{
-    type:String,
-    required:true,
-    enum:["KG", "Ltr", "Pcs"]
-
+  unit: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["kg", "litre", "pcs"],
+      message: "unit value can't be {VALUE}, must be kg/litre/pcs"
+    }
   },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [0, "quantity cant be negative"],
+    validate: {
+      validator: (value) => {
+        const isInteger = Number.isInteger(value);
+        if (isInteger) {
+          return true
+        } else {
+          return false
+        }
+      }
+    },
+    message: "Qunatity must be an integer"
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["in-stock", "out-of-stock", "discontinued"],
+      message: "status can't be {VALUE}"
+    }
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    detfault: Date.now
+  },
+  supplier: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Supplier"
+  },
+  categories: [{
+    name: {
+      type: String,
+      required: true
+    },
+    _id: mongoose.Schema.Types.ObjectId
+  }]
+}, {
+  timestamps: true,
 })
 
 app.get("/", (req, res) => {
